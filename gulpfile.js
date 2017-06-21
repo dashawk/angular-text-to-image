1fa3b237056cdd7dea6add89725ed3b5777799a1
Jason P <jmpanugaling@gmail.com>
2017-06-21 17:30:32 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var pump = require('pump');
+var $ = require('gulp-load-plugins')({ lazy: true });
+
+gulp.task('demo', function (cb) {
+	pump([
+		gulp.src('./src/angular-text-image.js'),
+		//$.uglify(),
+		$.ngAnnotate({ add: true }),
+		$.rename({ suffix: '.min' }),
+		gulp.dest('./demo')
+	], cb);
+});
---
+++ index.js
@@ -0,0 +1,26 @@
+var serve = require('serve');
+var port = process.env.PORT || 5000;
+
+var server = serve(__dirname + '/demo/', {
+	port: port,
+	ignore: ['node_modules']
+});
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+	if (key && key.ctrl && key.name === 'c') {
+		process.stdin.pause();
+		server.stop();
+		console.log('Exiting...');
+		process.exitCode = 0;
+	}
+});
+process.stdin.on('exit', function () {
+	process.exitCode = 0;
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
